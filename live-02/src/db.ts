dfcb1483106c546c1a6102d392e20b53b31ab671
Celina Takeda <takedacarina@gmail.com>
2021-10-20 15:32:25 -0300

    commit

--- live-02/src/db.ts
+++ live-02/src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = '';
+const connectionString = 'postgres://'; // pegar a URL do banco de dados no postgres
 
 const db = new Pool({ connectionString });
 
--- live-02/src/routes/users.route.ts
+++ live-02/src/routes/users.route.ts
@@ -1,25 +1,27 @@
 import  {Router, Request, Response, NextFunction } from "express";
 import { StatusCodes} from 'http-status-codes';
+import userRepository from "../repositories/user.repository";
 
 const usersRoute = Router();
 
-usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
-    const users = [{ userName: 'Celina' }];
+usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction) => {
+    const users = await userRepository.findAllUsers();
     res.status(StatusCodes.OK).send(users);
 
 });
 
-usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.get('/users/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    res.status(StatusCodes.OK).send({ uuid });
+    const user = await userRepository.findById(uuid);
+    res.status(StatusCodes.OK).send({ user });
 });
 
-usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction) => {
     const newUser = req.body;
     res.status(StatusCodes.CREATED).send(newUser);
 });
 
-usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.put('/users/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
     const modifiedUser = req.body;
 
@@ -28,7 +30,7 @@ usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, n
     res.status(StatusCodes.OK).send({ uuid });
 });
 
-usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {    
+usersRoute.delete('/users/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {    
     res.sendStatus(StatusCodes.OK);
 });
 
